e99b380dcae09ced983c1dcc6641f328752da0d3
abritopach <adrbrpa1988@gmail.com>
2019-07-10 15:51:51 +0200

    Added capacitor youtube player plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { YoutubePlayerWeb } from 'capacitor-youtube-player';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
 
-  constructor() {}
+  currentYear = new Date().getFullYear();
+
+  constructor() {
+    console.log('HomePage::constructor() | method called');
+  }
+
+  ngOnInit() {
+    console.log('HomePage::ngOnInit() | method called');
+  }
+
+  ngAfterViewInit() {
+    this.initializeYoutubePlayerPluginWeb();
+  }
+
+  async initializeYoutubePlayerPluginWeb() {
+    console.log('HomePage::initializeYoutubePlayerPluginWeb() | method called');
+    const options = {playerId: 'youtube-player', playerSize: {width: 640, height: 360}, videoId: 'tDW2C6rcH6M'};
+    const result = await YoutubePlayerWeb.initialize(options);
+    console.log('playerReady', result);
+  }
+
+  async destroyYoutubePlayerPluginWeb() {
+    console.log('HomePage::destroyYoutubePlayerPluginWeb() | method called');
+    const result = await YoutubePlayerWeb.destroy('youtube-player');
+    console.log('destroyYoutubePlayer', result);
+  }
 
 }
